1e203ab1d9344822f9aea56f27cbb7cd89e69d9b
zoe-L <xinyel1@andrew.cmu.edu>
2021-11-29 02:56:40 -0500

    Fix bug

--- cli/start.py
+++ cli/start.py
@@ -4,16 +4,16 @@ def main():
   if len(sys.argv)==2:
     print(len(sys.argv[1]))
     if sys.argv[1][0] == "1":
-      print("Apache Hadoop selected. Go to http://35.222.102.35/ for the application.")
+      print("Apache Hadoop selected. Go to http://34.71.159.122/ for the application.")
       exit()
     if sys.argv[1][0] == "2":
-      print("Apache Spark selected. Go to http://34.71.159.122/ for the application.")
+      print("Apache Spark selected. Go to http://35.232.52.139/ for the application.")
       exit()
     if sys.argv[1][0] == "3":
       print("Jupyter Notebook selected. Go to http://35.222.102.35/ for the application.")
       exit()
     if sys.argv[1][0] == "4":
-      print("SonarQube and SonnarScanner selected. Go to http://34.135.248.131/ for the application.")
+      print("SonarQube and SonnarScanner selected. Go to http://34.122.96.82/ for the application.")
       exit()
   print("command not recognized")
   exit()
--- cli/start.sh
+++ cli/start.sh
@@ -1,11 +1,3 @@
-gcloud container clusters create zoe-li-cluster \
-  --scopes "cloud-platform" \
-  --num-nodes 2
-gcloud config set container/cluster zoe-li-cluster
-gcloud container clusters get-credentials zoe-li-cluster
-kubectl create -f jupyter-notebook.yaml
-
-
 echo 'Welcome to Big Data Processing Application'
 echo 'Please type the number that corresponds to which application you would like to run:'
 echo '1. Apache Hadoop'
